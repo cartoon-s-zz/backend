fe76f14c034c9f684e6901fe0b26907122b0a6a7
Phadtharaded (B57) Sawankachirdwilai <phadtharaded-sawan.toon@hotmail.com>
2019-10-20 00:00:29 +0700

    added presentation and clean code

--- controller/scgController.js
+++ controller/scgController.js
@@ -55,18 +55,12 @@ exports.findResByPlace = function (req, res) {
   let YOUR_API_KEY = 'AIzaSyB5IlVNYMbdKdh-Ika4ZvnOCztHWhItyKw';
   let lat = 13.810577;
   let lng = 100.530278;
-  // let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=${lat},${lng}radius=10000&key=${YOUR_API_KEY}`;
-  // let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1000&type=restaurant&key=${YOUR_API_KEY}`
-  let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+BangSue&type=restaurant&key=${YOUR_API_KEY}`
-  // console.log(url);
+  let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+BangSue&type=restaurant&key=${YOUR_API_KEY}`;
   let data = [];
   request(url, function (error, response, body) {
     if (error) {
       throw error; //error happen
     }
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('data=====>', body)
     var obj = JSON.parse(body);
     for (let i of obj.results) {
       console.log(i.name);
@@ -78,15 +72,12 @@ exports.findResByPlace = function (req, res) {
       data.push(temp);
     }
     let url2 = `https://maps.googleapis.com/maps/api/place/textsearch/json?location=${lat},${lng}&type=restaurant&keyword=Bang Sue&key=${YOUR_API_KEY}`
-    // let url2 = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+BangSue&type=restaurant&key=${YOUR_API_KEY}`
-
     request(url2, function (error, response, body) {
       if (error) {
         console.log(error);
         throw error; //error happen
       }
       var obj = JSON.parse(body);
-      // console.log(body);
       for (let i of obj.results) {
         console.log(i.name);
         let temp = {
@@ -96,7 +87,6 @@ exports.findResByPlace = function (req, res) {
         }
         data.push(temp);
       }
-      // console.log('data', _.uniqBy(data))
       return res.send(_.uniqBy(data, 'name'));
     });
   });
