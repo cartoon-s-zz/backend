45624440d0d7c47f4705bd1af18cc53f068667ff
Phadtharaded (B57) Sawankachirdwilai <phadtharaded-sawan.toon@hotmail.com>
2019-10-19 18:36:13 +0700

    adjust res api

--- controller/scgController.js
+++ controller/scgController.js
@@ -53,22 +53,23 @@ exports.findParameter = function (req, res) {
 
 exports.findResByPlace = function (req, res) {
   let YOUR_API_KEY = 'AIzaSyB5IlVNYMbdKdh-Ika4ZvnOCztHWhItyKw';
-  let lat = 13.804983;
-  let lng = 100.536200;
+  let lat = 13.810577;
+  let lng = 100.530278;
   // let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=${lat},${lng}radius=10000&key=${YOUR_API_KEY}`;
   // let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1000&type=restaurant&key=${YOUR_API_KEY}`
   let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+BangSue&type=restaurant&key=${YOUR_API_KEY}`
-  console.log(url);
+  // console.log(url);
   let data = [];
   request(url, function (error, response, body) {
     if (error) {
       throw error; //error happen
     }
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log('data=====>', body)
     var obj = JSON.parse(body);
     for (let i of obj.results) {
+      console.log(i.name);
       let temp = {
         vicinity: i.formatted_address,
         rating: i.rating,
@@ -76,25 +77,27 @@ exports.findResByPlace = function (req, res) {
       }
       data.push(temp);
     }
-    // let url2 = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat + 0.001},${lng + 0.015}&radius=1000&type=restaurant&key=${YOUR_API_KEY}`
+    let url2 = `https://maps.googleapis.com/maps/api/place/textsearch/json?location=${lat},${lng}&type=restaurant&keyword=Bang Sue&key=${YOUR_API_KEY}`
     // let url2 = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+BangSue&type=restaurant&key=${YOUR_API_KEY}`
 
-    // request(url2, function (error, response, body) {
-    //   if (error) {
-    //     throw error; //error happen
-    //   }
-    //   var obj = JSON.parse(body);
-    //   // console.log(body);
-    //   for (let i of obj.results) {
-    //     // console.log(i.name);
-    //     let temp = {
-    //       vicinity: i.vicinity,
-    //       rating: i.rating,
-    //       name: i.name
-    //     }
-    //     data.push(temp);
-    //   }
-    return res.send(_.uniqBy(data));
-    // });
+    request(url2, function (error, response, body) {
+      if (error) {
+        console.log(error);
+        throw error; //error happen
+      }
+      var obj = JSON.parse(body);
+      // console.log(body);
+      for (let i of obj.results) {
+        console.log(i.name);
+        let temp = {
+          vicinity: i.formatted_address,
+          rating: i.rating,
+          name: i.name
+        }
+        data.push(temp);
+      }
+      // console.log('data', _.uniqBy(data))
+      return res.send(_.uniqBy(data, 'name'));
+    });
   });
 }
